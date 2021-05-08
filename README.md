2b07f81d61e85e9b4c3836da71249b7e452f184a
lxhom <lxhomeis@gmail.com>
2021-05-08 17:04:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# sedbot
+
+This is a bot which responds to tweets with `$sedthis (sed magic)` with the sed magic applied to the tweet above the mention.
+
+This bot was written to be used with [deta](https://deta.sh) (with a 1 minute cron), but you can use it locally.
+
+You can run it yourself by making a [deta](https://deta.sh) account, changing the keys in `keys.template.js` to your keys and renaming the file to `keys.js`, deploying the code on deta and setting the cron to 1 minute. You can run it locally by doing `npm install` and then replacing a few lines of code in index.js:
+```diff
+- app.lib.cron(getTaggedTweets)
+- module.exports = app
++ getTaggedTweets()
+```
+You need to call node index every minute for the bot to work.
---
+++ index.js
@@ -0,0 +1,44 @@
+let Twit = require("twit")
+let keys = require("./keys.js")
+let child_process = require("child_process")
+let fs = require("fs")
+let { app } = require("deta")
+
+let T = new Twit(keys.twitter)
+
+let getTaggedTweets = async () => {
+  let res = await T.get("search/tweets", {q: "$sedthis", result_type: "recent"})
+  console.log(res.data.statuses)
+  for (let status of res.data.statuses) {
+    console.log(getDateSecondsAge(status.created_at))
+    if (getDateSecondsAge(status.created_at) <= 120) {
+      try { await interpretTweet(status) } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+}
+
+let interpretTweet = async status => {
+  let scriptTweet = status.text;
+  let scriptIndex = scriptTweet.match(/\$sedthis/).index + "$sedthis".length + 1;
+  let script = scriptTweet.substr(scriptIndex)
+
+  let text = (await T.get("statuses/show", {id: status.in_reply_to_status_id_str})).data.text;
+
+  let output = execSed(text, script).replace(/\$sedthis/g, "#sedthis")
+
+  await T.post("statuses/update", {status: output, in_reply_to_status_id: status.id_str, auto_populate_reply_metadata: true});
+}
+
+let getDateSecondsAge = string => (Date.now() - new Date(string)) / 1000
+
+let execSed = (text, script) => {
+  fs.writeFileSync("/tmp/t", text);
+  fs.writeFileSync("/tmp/s", script);
+  return child_process.execSync("sed -f /tmp/s /tmp/t").toString()
+}
+
+// deta stuff (replace this with getTaggedTweets() if you want to run it locally
+app.lib.cron(getTaggedTweets)
+module.exports = app
